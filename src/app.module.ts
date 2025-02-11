7240941b88cfae9eae54c8305fbd31d6ff848197
ismaelrr888 <ismaelrobaina@aivo.co>
2025-02-11 11:41:19 +0200

    implement user management module with create, update, and delete functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://ismaelrr888:lautrivHUA2020@cluster0.lk1uj.mongodb.net/db?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
